558576ac5b29bc127d9f9105d52a4894555a5f88
javierduarte69420 <javierduarte69420@hotmail.com>
2026-02-02 20:37:57 -0600

    Update index.tsx

--- src/sections/Admin/index.tsx
+++ src/sections/Admin/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { AdminPanel } from './components/AdminPanel';
-import type { VerificationResult } from '@/App';
+import React from "react";
+import { AdminPanel } from "./components/AdminPanel";
+import type { VerificationResult } from "@/App";
 
 export type AdminProps = {
   verificationResult: VerificationResult | null;
