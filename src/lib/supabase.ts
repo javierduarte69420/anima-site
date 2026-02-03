c33fc9c9ee9b1b830171b1c68244cabf79508a47
javierduarte69420 <javierduarte69420@hotmail.com>
2026-02-02 21:58:38 -0600

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://xtzodrwoktumqjjkmntm.supabase.co'
-const supabaseAnonKey = 'sb_publishable_140GG4HVO5ht8z791mK5Ng_i24EXk80'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
-)
+);
